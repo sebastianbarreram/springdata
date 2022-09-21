19fd044686739285e54b8a6c8858811fb0e00df4
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-21 16:42:44 -0500

    Init springboot project

---
+++ src/main/java/com/example/HolaSpring/Persona.java
@@ -0,0 +1,11 @@
+package com.example.HolaSpring;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
